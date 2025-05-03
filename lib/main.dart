b95bc0cd35d997a8744d8c3803a426aa2cef7eaa
AdrianGarciaB <adriangarciabel@gmail.com>
2025-05-03 12:15:45 +0200

    Ultimos cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
-// import 'package:better_open_file/better_open_file.dart';
 import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:camerawesome/pigeon.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'utils/file_utils.dart';
+import 'package:http/http.dart' as http;
+import 'dart:io';
 
 void main() {
   runApp(const CameraAwesomeApp());
@@ -23,11 +25,69 @@ class CameraAwesomeApp extends StatelessWidget {
   }
 }
 
-class CameraPage extends StatelessWidget {
+class CameraPage extends StatefulWidget {
   const CameraPage({super.key});
 
   @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  bool _cameraPermissionGranted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAndRequestPermission();
+  }
+
+  Future<void> _checkAndRequestPermission() async {
+    final status = await Permission.camera.status;
+
+    if (status.isGranted) {
+      setState(() {
+        _cameraPermissionGranted = true;
+      });
+    } else {
+      final result = await Permission.camera.request();
+      if (result.isGranted) {
+        setState(() {
+          _cameraPermissionGranted = true;
+        });
+      } else {
+        // Puedes mostrar un diálogo o redirigir al usuario
+        debugPrint('Permiso de cámara denegado');
+      }
+    }
+  }
+
+
+
+  Future<void> uploadImage(File imageFile) async {
+    var uri = Uri.parse('http://172.20.10.4:8000/upload-image/');
+
+    var request = http.MultipartRequest('POST', uri)
+      ..files.add(await http.MultipartFile.fromPath('file', imageFile.path));
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      print('Imagen enviada con éxito');
+    } else {
+      print('Fallo al enviar la imagen');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (!_cameraPermissionGranted) {
+      return const Scaffold(
+        body: Center(
+          child: Text('Esperando permiso de cámara...'),
+        ),
+      );
+    }
+
     return Scaffold(
       body: Container(
         color: Colors.white,
@@ -39,6 +99,7 @@ class CameraPage extends StatelessWidget {
               case (MediaCaptureStatus.success, true, false):
                 event.captureRequest.when(
                   single: (single) {
+                    uploadImage(File(single.file!.path));
                     debugPrint('Picture saved: ${single.file?.path}');
                   },
                   multiple: (multiple) {
@@ -51,19 +112,6 @@ class CameraPage extends StatelessWidget {
                 debugPrint('Failed to capture picture: ${event.exception}');
               case (MediaCaptureStatus.capturing, false, true):
                 debugPrint('Capturing video...');
-              case (MediaCaptureStatus.success, false, true):
-                event.captureRequest.when(
-                  single: (single) {
-                    debugPrint('Video saved: ${single.file?.path}');
-                  },
-                  multiple: (multiple) {
-                    multiple.fileBySensor.forEach((key, value) {
-                      debugPrint('multiple video taken: $key ${value?.path}');
-                    });
-                  },
-                );
-              case (MediaCaptureStatus.failure, false, true):
-                debugPrint('Failed to capture video: ${event.exception}');
               default:
                 debugPrint('Unknown event: $event');
             }
@@ -79,7 +127,6 @@ class CameraPage extends StatelessWidget {
                     '${testDir.path}/${DateTime.now().millisecondsSinceEpoch}.jpg';
                 return SingleCaptureRequest(filePath, sensors.first);
               }
-              // Separate pictures taken with front and back camera
               return MultipleCaptureRequest(
                 {
                   for (final sensor in sensors)
@@ -88,7 +135,7 @@ class CameraPage extends StatelessWidget {
                 },
               );
             },
-            exifPreferences: ExifPreferences(saveGPSLocation: true),
+            exifPreferences: ExifPreferences(saveGPSLocation: false),
           ),
           sensorConfig: SensorConfig.single(
             sensor: Sensor.position(SensorPosition.back),
@@ -97,7 +144,6 @@ class CameraPage extends StatelessWidget {
             zoom: 0.0,
           ),
           enablePhysicalButton: true,
-          // filter: AwesomeFilter.AddictiveRed,
           previewAlignment: Alignment.center,
           previewFit: CameraPreviewFit.contain,
           onMediaTap: (mediaCapture) {
